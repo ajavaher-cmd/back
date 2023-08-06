b0aabfea79a186235285a2860809fb09f3dc62a3
ajavaher-cmd <ajavaher@ualberta.ca>
2023-08-06 14:04:14 -0400

    static servenocors

--- back/index.js
+++ back/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 var cors = require('cors');
 const app = express()
 
-app.use(cors());
+// app.use(cors());
 app.use(express.json())
 app.use(express.static('build'))
 
